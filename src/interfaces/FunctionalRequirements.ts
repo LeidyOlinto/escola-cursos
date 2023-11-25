12b8756e4fa4d47cd3385782839ebb0a7d3133c4
Igor de Andrade Lopes <96532725+IgorALopes@users.noreply.github.com>
2023-11-25 19:06:20 -0300

    Created FunctionalRequirements interface for class menu

---
+++ src/interfaces/FunctionalRequirements.ts
@@ -0,0 +1,19 @@
+export interface FunctionalRequirements {
+  start(): void
+  manageStudents(): void
+  manageDisciplines(): void
+  manageCourses(): void
+  registerStudent(): void
+  conferStudent(): void
+  deleteStudent(): void
+  updateStudent(): void
+  registerDiscipline(): void
+  conferDiscipline(): void
+  deleteDiscipline(): void
+  updateDiscipline(): void
+  registerCourse(): void
+  conferCourse(): void
+  deleteCourse(): void
+  updateCourse(): void
+  sair(): void
+}
