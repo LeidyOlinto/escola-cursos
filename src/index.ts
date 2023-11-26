05213dc90aeeac7b798c199493294b260eef6c2f
leidyolinto <suverleide.olinto@gmail.com>
2023-11-26 20:29:04 -0300

    new objets for tests

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,17 @@ import { Menu } from './entities/Menu'
 const main = () => {
     new Menu().start()
 }
+const d1 = new Discipline ("back end" , 18, 10);
+const d2 = new Discipline ("Logica de Programação", 25,90);
+const d3 = new Discipline ("Banco de dados", 30,80);
+const d4 = new Discipline ("MVC", 20,60);
+const L1 = [ d1,d2]
+const L2 = [d3, d4]
+const C1 = new Course ("TI dados",L1 );
+const C2 = new Course ( "Dados",[...L2 ,...L1]);
 
-const listCourses = [new Course("Back end", [new Discipline("lógica", 20), new Discipline("POO", 30)])]
+
+
 
 // TESTE COM DADOS
 
