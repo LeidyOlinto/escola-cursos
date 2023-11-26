fa1b09f5343c436786621b0ae78b2b7b207ea8fb
Djair Alves <djair.dj@gmail.com>
2023-11-26 18:01:08 -0300

    feat: Update Utils.ts

--- src/entities/Utils.ts
+++ src/entities/Utils.ts
@@ -1,9 +1,18 @@
+import { Discipline } from "./Discipline";
 import { Student } from "./Student";
 
-export function findStudentByName(students: Student[], name: string): Student | undefined {
+function findStudentByName(students: Student[], name: string): Student | undefined {
     return students.find(student => student.getName() === name);
 }
 
-export function findStudentIndexByName(students: Student[], name: string): number {
+function findStudentIndexByName(students: Student[], name: string): number {
     return students.findIndex(student => student.getName() === name);
 }
+function findDisciplineByName(disciplines: Discipline[], name: string): Discipline | undefined {
+    return disciplines.find(discipline => discipline.getName() == name);
+}
+
+function findDisciplineIndexByName(disciplines: Discipline[], name: string): number {
+    return disciplines.findIndex(discipline => discipline.getName() === name);
+}
+export { findStudentByName, findStudentIndexByName, findDisciplineByName , findDisciplineIndexByName}
