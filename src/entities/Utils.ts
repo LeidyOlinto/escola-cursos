35d8cba6edf6df4080cc9564c4c8721e8a01480d
Djair Alves <djair.dj@gmail.com>
2023-11-26 23:52:47 -0300

    style: Formatação

--- src/entities/Utils.ts
+++ src/entities/Utils.ts
@@ -2,18 +2,19 @@ import { Discipline } from "./Discipline";
 import { Student } from "./Student";
 
 function findStudentByName(students: Student[], name: string): Student | undefined {
-    return students.find(student => student.getName() === name);
+    return students.find(student => student.getName().toUpperCase() === name.toUpperCase());
 }
 
 function findStudentIndexByName(students: Student[], name: string): number {
-    return students.findIndex(student => student.getName() === name);
+    return students.findIndex(student => student.getName().toUpperCase() === name.toUpperCase());
 }
+
 function findDisciplineByName(disciplines: Discipline[], name: string): Discipline | undefined {
-    return disciplines.find(discipline => discipline.getName() == name);
+    return disciplines.find(discipline => discipline.getName().toUpperCase() == name.toUpperCase());
 }
 
 function findDisciplineIndexByName(disciplines: Discipline[], name: string): number {
-    return disciplines.findIndex(discipline => discipline.getName() === name);
+    return disciplines.findIndex(discipline => discipline.getName().toUpperCase() === name.toUpperCase());
 }
 
 export { findStudentByName, findStudentIndexByName, findDisciplineByName , findDisciplineIndexByName}
